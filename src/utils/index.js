36be88d78c81f7d87a19745421ed358d6fce2e0b
Pedro Tashima <pedrotashima42@gmail.com>
2021-06-18 19:35:52 -0300

    chore: parse question when enter the component

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,22 +5,26 @@ const questions = {}
 
 for (const topic in rawQuestions) {
   rawQuestions[topic].forEach(question => {
-    const parser = new QuestionParser(question.text)
     if (questions[topic] === undefined) {
       questions[topic] = []
     }
-    questions[topic].push({
-      id: question.id,
-      title: question.title,
-      text: parser.getText(),
-      answer: question.answer.toString(),
-      subject: question.subject,
-      level: question.level,
-      tags: question.tags,
-      values: parser.getAllValues(),
-      //type: question.type,
-    })
+    questions[topic].push(parseQuestion(question))
   })
 }
 
-export { questions }
+function parseQuestion(question) {
+  const parser = new QuestionParser(question.text)
+  return {
+    id: question.id,
+    title: question.title,
+    text: parser.getText(),
+    answer: question.answer.toString(),
+    subject: question.subject,
+    level: question.level,
+    tags: question.tags,
+    values: parser.getAllValues(),
+    //type: question.type,
+  }
+}
+
+export { questions, rawQuestions, parseQuestion }
