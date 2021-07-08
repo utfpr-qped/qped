bc1245170b8067f1c3d3fe0c58202229482aaa6d
Pedro Tashima <pedrotashima42@gmail.com>
2021-07-08 17:16:38 -0300

    feat: added history hashing

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
 import './index.css';
 
+import { runHash } from "../../utils/hash.js"
+
 const Navbar = () => {
   const handleDownload = () => {
     //TODO: this function and the download button is only for testing
     const element = document.createElement("a");
-    const blob = new Blob([localStorage.getItem('history')], {type: 'application/json'}); //pass data from localStorage API to blob
+    const history = localStorage.getItem('history')
+    const hash = runHash(history)
+    const exportHistory = JSON.stringify({ hash, history: JSON.parse(history) })
+    const blob = new Blob([exportHistory], { type: 'application/json' }); //pass data from localStorage API to blob
     element.href = URL.createObjectURL(blob);
     element.download = "history.json";
     element.click();
@@ -15,7 +20,7 @@ const Navbar = () => {
     <nav className="sidenav">
       <div className="brand">
         <div className="h4">QPED</div>
-        <span>Questões para <br/>Estrutura de Dados</span>
+        <span>Questões para <br />Estrutura de Dados</span>
       </div>
 
       <div className="nav-menu">
@@ -23,7 +28,7 @@ const Navbar = () => {
         <Link to="/topics" className="text-light">Tópicos</Link>
         <Link to="/about" className="text-light">Sobre</Link>
         <Link to="/help" className="text-light">Ajuda</Link>
-        { localStorage.getItem('history') && <Link to="/" className="text-muted" onClick={handleDownload}>Baixar Histórico</Link> }
+        {localStorage.getItem('history') && <Link to="/" className="text-muted" onClick={handleDownload}>Baixar Histórico</Link>}
       </div>
     </nav>
   );
